f02d7c34d78554015b2c7188dfbd03de1ec1a5d7
Дониёр Асадуллаев <asadullaevdonier@gmail.com>
2023-09-11 18:59:40 +0500

    Completed form

---
+++ app/Http/Controllers/SendFormController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SendFormController extends Controller
+{
+    function send(Request $request) 
+    {
+        dd($request);
+        
+        
+    }
+}
